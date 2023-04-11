72ed0ff6a89923256bda9f3cb097663beb4c838f
Jeferson Cruz Cordova <jeferson.cruz@unmsm.edu.pe>
2023-04-11 12:39:42 -0500

    Update scriptBotonesVerTabla.js

--- scriptBotonesVerTabla.js
+++ scriptBotonesVerTabla.js
@@ -28,6 +28,13 @@ boton2.addEventListener('click', () => {
   
 });
 
+boton2CLASE.addEventListener('click', () => {
+  mostrarVentanaEmergente();
+  mostrarTabla2CLASE();
+  
+});
+
+
 ventanaEmergente.addEventListener('click', (event) => {
   if (event.target === ventanaEmergente) {
     ocultarVentanaEmergente();
@@ -47,7 +54,7 @@ function mostrarTabla1() {
   const fila1 = tabla.insertRow();
   const celda11 = fila1.insertCell();
   const celda12 = fila1.insertCell();
-  celda11.textContent = "Presentación(PPT)_Semana 01_Sistemas Inteligentes_Grupo.pdf";
+  celda11.textContent = "Semana 01_Sistemas Inteligentes_Grupo 3.pdf";
 
   const enlace1 = document.createElement("a");
   enlace1.href = enlaces[0];
@@ -95,11 +102,11 @@ function mostrarTabla1CLASE() {
   celda22.appendChild(enlace2);
 }
 
-// Función para mostrar la tabla 2
+// Función para mostrar la tabla 1
 function mostrarTabla2() {
   const enlaces = [
-    'https://docs.google.com/spreadsheets/d/1VFTTKeHW7Ua-3kr6W1v8jAK85VxMiDPW/edit?usp=sharing&ouid=106095924712021991417&rtpof=true&sd=true',
-    'https://docs.google.com/spreadsheets/d/1NReRaMURU_hVgwCBPr2MsbL2OSRxsl6s/edit?usp=sharing&ouid=106095924712021991417&rtpof=true&sd=true'
+    "https://drive.google.com/file/d/1mrFkO0MzWEBzTUfOzg9m6EoKh5xzIhyC/view",
+    "https://drive.google.com/file/d/1P3Cpd4YXciA6dKS_cevTWlMbbn1rbkS_/view"
   ];
   const tabla = document.getElementById('tablaDinamica');
   // Borrar todas las filas de la tabla
@@ -108,30 +115,27 @@ function mostrarTabla2() {
   const fila1 = tabla.insertRow();
   const celda11 = fila1.insertCell();
   const celda12 = fila1.insertCell();
-  celda11.textContent = "";
+  celda11.textContent = "Semana 02_Sistemas Inteligentes_Grupo 3.pdf";
+
   const enlace1 = document.createElement("a");
   enlace1.href = enlaces[0];
   enlace1.target = "_blank";
   enlace1.textContent = "Ir al archivo";
   celda12.appendChild(enlace1);
 
-  // Crear segunda fila
-  const fila2 = tabla.insertRow();
-  const celda21 = fila2.insertCell();
-  const celda22 = fila2.insertCell();
-  celda21.textContent = "tarea2.xlsx";
-  const enlace2 = document.createElement("a");
-  enlace2.href = enlaces[1];
-  enlace2.target = "_blank";
-  enlace2.textContent = "Ir al archivo";
-  celda22.appendChild(enlace2);
+    // Crear segunda fila
+    const fila2 = tabla.insertRow();
+    const celda21 = fila2.insertCell();
+    const celda22 = fila2.insertCell();
+    celda21.textContent = "Presentación del Tema_Sistemas Inteligentes_Grupo 3.pdf";
+  
+    const enlace2 = document.createElement("a");
+    enlace2.href = enlaces[1];
+    enlace2.target = "_blank";
+    enlace2.textContent = "Ir al archivo";
+    celda22.appendChild(enlace2);
 
 }
-
-
-
-
-
 
 function ocultarVentanaEmergente() {
   ventanaEmergente.style.display = 'none';
